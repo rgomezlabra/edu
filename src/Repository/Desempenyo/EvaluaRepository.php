28124fd66fe87c01b77b367c3f0e76a0c0eda584
RAMON MARIA GOMEZ LABRADOR <ramongomez@us.es>
2024-10-14 12:52:36 +0200

    #39647: Mejorar las consultas de evaluaciones permitidas en Desempeño.

--- src/Repository/Desempenyo/EvaluaRepository.php
+++ src/Repository/Desempenyo/EvaluaRepository.php
@@ -6,6 +6,7 @@ use App\Entity\Cuestiona\Cuestionario;
 use App\Entity\Desempenyo\Evalua;
 use App\Entity\Plantilla\Empleado;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -42,19 +43,6 @@ class EvaluaRepository extends ServiceEntityRepository
         $this->getEntityManager()->flush();
     }
 
-    /** Borrar datos de autoevaluación. */
-    public function deleteAutoevaluacion(Cuestionario $cuestionario): void
-    {
-        $this->createQueryBuilder('evalua')
-            ->delete()
-            ->andWhere('evalua.cuestionario = :cuestionario')
-            ->andWhere('evalua.empleado = evalua.evaluador')
-            ->setParameter('cuestionario', $cuestionario)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-
     /**
      * Buscar datos de evaluación para los criterios de búsqueda indicados (cuestionario, empleado, evaluador y tipo de
      * evaluación).
@@ -63,32 +51,102 @@ class EvaluaRepository extends ServiceEntityRepository
      */
     public function findByEvaluacion(array $criterios): array
     {
-        $qb = $this->createQueryBuilder('evalua');
+        $qb = $this->createQueryBuilder('evalua')
+            ->addSelect('origen')
+            ->join('evalua.origen', 'origen')
+        ;
+        $empleados = false;
+        $evaluaciones = false;
         foreach ($criterios as $criterio => $valor) {
             switch ($criterio) {
                 case 'cuestionario':
-                    $qb->join('evalua.cuestionario', 'cuestionario')
-                        ->andWhere('cuestionario.id = :cuestionario')
-                        ->setParameter('cuestionario', $valor->getId())
-                    ;
+                    if ($valor instanceof Cuestionario) {
+                        $qb->join('evalua.cuestionario', 'cuestionario')
+                            ->andWhere('cuestionario.id = :cuestionario')
+                            ->setParameter('cuestionario', $valor->getId())
+                        ;
+                    }
                     break;
                 case 'empleado':
-                    $qb->join('evalua.empleado', 'empleado')
-                        ->andWhere('empleado.id = :empleado')
-                        ->setParameter('empleado', $valor->getId())
-                    ;
+                    if ($valor instanceof Empleado) {
+                        $empleados = true;
+                        $qb->andWhere('empleado.id = :empleado')
+                            ->setParameter('empleado', $valor->getId())
+                        ;
+                    }
                     break;
                 case 'evaluador':
-                    $qb->join('evalua.evaluador', 'evaluador')
-                        ->andWhere('evaluador.id = :evaluador')
-                        ->setParameter('evaluador', $valor->getId())
-                    ;
+                    if ($valor instanceof Empleado) {
+                        $evaluaciones = true;
+                        $qb->andWhere('evaluador.id = :evaluador')
+                            ->setParameter('evaluador', $valor->getId())
+                        ;
+                    }
                     break;
                 case 'tipo':
+                    /** @var int[] $valor */
+                    if (!is_array($valor)) {
+                        $valor = [$valor];
+                    }
+
+                    if (in_array(Evalua::AUTOEVALUACION, $valor) || in_array(Evalua::NO_EVALUACION, $valor)) {
+                        $empleados = true;
+                    } elseif (in_array(Evalua::EVALUA_RESPONSABLE, $valor) || in_array(Evalua::EVALUA_OTRO, $valor)) {
+                        $empleados = true;
+                        $evaluaciones = true;
+                    }
+
                     $qb->andWhere('evalua.tipo_evaluador IN (:tipo)')->setParameter('tipo', $valor);
             }
         }
 
+        if ($empleados) {
+            $qb = $this->addEmpleados($qb);
+        }
+        if ($evaluaciones) {
+            $qb = $this->addEvaluadores($qb);
+        }
+
         return $qb->getQuery()->getResult();
+    }
+
+    /** Mejorar consulta para obtener datos de empleado evaluado. */
+    private function addEmpleados(QueryBuilder $qb): QueryBuilder
+    {
+        return $qb->addSelect('empleado')
+            ->addSelect('empleado_persona')
+            ->addSelect('empleado_grupo')
+            ->addSelect('empleado_plaza_titular')
+            ->addSelect('empleado_unidad_titular')
+            ->addSelect('empleado_plaza_ocupada')
+            ->addSelect('empleado_unidad_ocupada')
+            ->join('evalua.empleado', 'empleado')
+            ->join('empleado.persona', 'empleado_persona')
+            ->join('empleado.grupo', 'empleado_grupo')
+            ->leftJoin('empleado.plaza_titular', 'empleado_plaza_titular')
+            ->leftJoin('empleado_plaza_titular.unidad', 'empleado_unidad_titular')
+            ->leftJoin('empleado.plaza_ocupada', 'empleado_plaza_ocupada')
+            ->leftJoin('empleado_plaza_ocupada.unidad', 'empleado_unidad_ocupada')
+        ;
+    }
+
+    /** Mejorar consulta para obtener datos de evaluador. */
+    private function addEvaluadores(QueryBuilder $qb): QueryBuilder
+    {
+        return $qb->addSelect('evaluador')
+            ->addSelect('evaluador_persona')
+            ->addSelect('evaluador_grupo')
+            ->addSelect('evaluador_plaza_titular')
+            ->addSelect('evaluador_unidad_titular')
+            ->addSelect('evaluador_plaza_ocupada')
+            ->addSelect('evaluador_unidad_ocupada')
+            ->join('evalua.evaluador', 'evaluador')
+            ->join('evaluador.persona', 'evaluador_persona')
+            ->join('evaluador.grupo', 'evaluador_grupo')
+            ->leftJoin('evaluador.plaza_titular', 'evaluador_plaza_titular')
+            ->leftJoin('evaluador_plaza_titular.unidad', 'evaluador_unidad_titular')
+            ->leftJoin('evaluador.plaza_ocupada', 'evaluador_plaza_ocupada')
+            ->leftJoin('evaluador_plaza_ocupada.unidad', 'evaluador_unidad_ocupada')
+        ;
     }
 }
